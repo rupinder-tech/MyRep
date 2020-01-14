3d617ddb5d632ee788b47b29a7e1ec8540938904
RupinderjeetSingh <rupinsi1@in.ibm.com>
2020-01-14 16:21:55 +0530

    Just for learn

--- Collection Practice/src/CollectionDemo/DemoList.java
+++ Collection Practice/src/CollectionDemo/DemoList.java
@@ -9,10 +9,10 @@ public class DemoList {
 	public static void main(String args[]) {
 		
 		List<String> l1 = new ArrayList<String>();
-		l1.add("ABC");
+		l1.add("IBM");
 		l1.add("AT&T");
 		
-		ListIterator<String> itr = l1.listIterator();
+		ListIterator<String> itr = l1.listIterator(); 
 		
 		while(itr.hasNext()) {
 			System.out.println("index : " + itr.nextIndex() + " value: " + itr.next());
